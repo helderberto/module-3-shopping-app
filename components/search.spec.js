4068847c61b148ee62322e3bb946cdff8a519322
Helder Burato Berto <helder.burato@gmail.com>
2021-11-27 11:26:44 +0000

    Change Search to use div like in tuto

--- components/search.spec.js
+++ components/search.spec.js
@@ -6,6 +6,6 @@ describe('<Search />', () => {
   it('should render Search component', () => {
     render(<Search />);
 
-    expect(screen.getByRole('form')).toBeInTheDocument();
+    expect(screen.getByTestId('search')).toBeInTheDocument();
   });
 });
