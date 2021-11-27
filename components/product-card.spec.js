ed13c33e4ca51ca91aa21c45a6aaee23312e729a
Helder Burato Berto <helder.burato@gmail.com>
2021-11-27 12:40:42 +0000

    Ensure ProductCard have the proper content

--- components/product-card.spec.js
+++ components/product-card.spec.js
@@ -2,10 +2,27 @@ import { render, screen } from '@testing-library/react';
 
 import ProductCard from './product-card';
 
+const product = {
+  title: 'Watch',
+  price: '22.00',
+  image:
+    'https://images.unsplash.com/photo-1622925566273-d7cfdcc34ec6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3024&q=80',
+};
+
 describe('<ProductCard />', () => {
   it('should render ProductCard component', () => {
-    render(<ProductCard />);
+    render(<ProductCard product={product} />);
 
     expect(screen.getByTestId('product-card')).toBeInTheDocument();
+  });
+
+  it('should display proper content', () => {
+    render(<ProductCard product={product} />);
+
+    expect(screen.getByText(new RegExp(product.title, 'i'))).toBeInTheDocument();
+    expect(screen.getByText(new RegExp(product.price, 'i'))).toBeInTheDocument();
+    expect(screen.getByTestId('image')).toHaveStyle({
+      backgroundImage: product.image,
+    });
   });
 });
