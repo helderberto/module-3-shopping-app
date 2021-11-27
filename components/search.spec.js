faf43d3e8e43430ffe7916994a39bc0ac7c6800a
Helder Burato Berto <helder.burato@gmail.com>
2021-11-27 11:59:19 +0000

    Ensure searchbox has been called with mock function

--- components/search.spec.js
+++ components/search.spec.js
@@ -1,11 +1,23 @@
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Search from './search';
 
-describe('<Search />', () => {
-  it('should render Search component', () => {
-    render(<Search />);
+const doSearch = jest.fn();
 
-    expect(screen.getByTestId('search')).toBeInTheDocument();
+describe('<Search />', () => {
+  it('should render a form', () => {
+    render(<Search doSearch={doSearch} />);
+
+    expect(screen.getByRole('form')).toBeInTheDocument();
+  });
+
+  it('should call doSearch() when form is submitted', () => {
+    render(<Search doSearch={doSearch} />);
+
+    const input = screen.getByRole('searchbox');
+    userEvent.type(input, '{enter}');
+
+    expect(doSearch).toHaveBeenCalledTimes(1);
   });
 });
