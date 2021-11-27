b39dffa255ed482bf2a225635bd499424d3ec8d1
Helder Burato Berto <helder.burato@gmail.com>
2021-11-27 16:04:04 +0000

    Cleanup files

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,8 @@
-/* istanbul ignore file */
 import '../styles/globals.css';
 
 import Cart from '../components/cart';
-import { useCartStore } from '../store/cart';
-
-if (process.env.NODE_ENV === 'development') {
-  require('../miragejs/server').makeServer();
-}
 
 function MyApp({ Component, pageProps }) {
-  const toggle = useCartStore((store) => store.actions.toggle);
-
   return (
     <div className="bg-white">
       <header>
@@ -38,14 +30,9 @@ function MyApp({ Component, pageProps }) {
               </svg>
               <span className="mx-1 text-sm">NY</span>
             </div>
-            <div className="w-full text-gray-700 md:text-center text-2xl font-semibold">
-              Brand
-            </div>
+            <div className="w-full text-gray-700 md:text-center text-2xl font-semibold">Brand</div>
             <div className="flex items-center justify-end w-full">
-              <button
-                onClick={() => toggle()}
-                className="text-gray-600 focus:outline-none mx-4 sm:mx-0"
-              >
+              <button className="text-gray-600 focus:outline-none mx-4 sm:mx-0">
                 <svg
                   className="h-5 w-5"
                   fill="none"
@@ -58,6 +45,7 @@ function MyApp({ Component, pageProps }) {
                   <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                 </svg>
               </button>
+
               <div className="flex sm:hidden">
                 <button
                   type="button"
@@ -76,34 +64,19 @@ function MyApp({ Component, pageProps }) {
           </div>
           <nav className="sm:flex sm:justify-center sm:items-center mt-4">
             <div className="flex flex-col sm:flex-row">
-              <a
-                className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0"
-                href="#"
-              >
+              <a className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">
                 Home
               </a>
-              <a
-                className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0"
-                href="#"
-              >
+              <a className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">
                 Shop
               </a>
-              <a
-                className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0"
-                href="#"
-              >
+              <a className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">
                 Categories
               </a>
-              <a
-                className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0"
-                href="#"
-              >
+              <a className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">
                 Contact
               </a>
-              <a
-                className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0"
-                href="#"
-              >
+              <a className="mt-3 text-gray-600 hover:underline sm:mx-3 sm:mt-0" href="#">
                 About
               </a>
             </div>
@@ -114,10 +87,7 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
       <footer className="bg-gray-200">
         <div className="container mx-auto px-6 py-3 flex justify-between items-center">
-          <a
-            href="#"
-            className="text-xl font-bold text-gray-500 hover:text-gray-400"
-          >
+          <a href="#" className="text-xl font-bold text-gray-500 hover:text-gray-400">
             Brand
           </a>
           <p className="py-2 text-gray-500 sm:py-0">All rights reserved</p>
