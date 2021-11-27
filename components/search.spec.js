27758808b202d9f28af92b2c483858eb3d393097
Helder Burato Berto <helder.burato@gmail.com>
2021-11-27 12:12:51 +0000

    Ensure search submit term

--- components/search.spec.js
+++ components/search.spec.js
@@ -1,4 +1,4 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Search from './search';
@@ -12,12 +12,32 @@ describe('<Search />', () => {
     expect(screen.getByRole('form')).toBeInTheDocument();
   });
 
+  it('should render a input equals to search', () => {
+    render(<Search doSearch={doSearch} />);
+
+    expect(screen.getByRole('searchbox')).toHaveProperty('type', 'search');
+  });
+
   it('should call doSearch() when form is submitted', () => {
     render(<Search doSearch={doSearch} />);
 
-    const input = screen.getByRole('searchbox');
-    userEvent.type(input, '{enter}');
+    const form = screen.getByRole('form');
+
+    fireEvent.submit(form);
 
     expect(doSearch).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call doSearch() with the user input', () => {
+    render(<Search doSearch={doSearch} />);
+
+    const inputText = 'any_text';
+    const form = screen.getByRole('form');
+    const input = screen.getByRole('searchbox');
+
+    userEvent.type(input, inputText);
+    fireEvent.submit(form);
+
+    expect(doSearch).toHaveBeenCalledWith(inputText);
   });
 });
