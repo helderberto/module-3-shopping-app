8302a0b6571f84aa3e2f204001e95bbc3a5d2110
Helder Burato Berto <helder.burato@gmail.com>
2021-12-01 19:13:28 +0000

    Ensure when trigger catch() return an error

--- hooks/use-fetch-products.unit.spec.js
+++ hooks/use-fetch-products.unit.spec.js
@@ -1,7 +1,7 @@
 import { renderHook } from '@testing-library/react-hooks';
 
 import { useFetchProducts } from './use-fetch-products';
-import { makeServer } from '../miragejs/server';
+import { makeServer, Response } from '../miragejs/server';
 
 describe('useFetchProducts', () => {
   let server;
@@ -23,5 +23,16 @@ describe('useFetchProducts', () => {
 
     expect(result.current.products).toHaveLength(10);
     expect(result.current.error).toBe(false);
+  });
+
+  it('should set error to true when catch() is executed', async () => {
+    server.get('products', () => new Response(500, {}, ''));
+
+    const { result, waitForNextUpdate } = renderHook(() => useFetchProducts());
+
+    await waitForNextUpdate();
+
+    expect(result.current.error).toBe(true);
+    expect(result.current.products).toHaveLength(0);
   });
 });
